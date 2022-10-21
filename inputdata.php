764df14eb79ea07071e460f0784a23872ddd7305
Angelo Dionisio <angelo.dionisio09@gmail.com>
2022-10-21 09:48:05 +0800

    validation
    function

--- inputdata.php
+++ inputdata.php
@@ -30,26 +30,47 @@
         $ContactNo = $_REQUEST['ContactNo'];
        
          
-        // Performing insert query execution
-        // here our table name is college
-        $sql = "INSERT INTO student  VALUES ('$Sr_code',
-            '$Lastname','$Firstname','$Middlename','$Address','$ContactNo')";
-         
+
+        $sql_validation = "SELECT * FROM student WHERE sr-code = '$Sr_code'";
         echo $sql;
-        if(mysqli_query($conn, $sql)){
-            echo "<h3>data stored in a database successfully."
-                . " Please browse your localhost php my admin"
-                . " to view the updated data</h3>";
- 
-            echo nl2br("\n$Sr_code\n $Lastname\n "
-                . "$Firstname\n $Middlename\n $Address\n $ContactNo\n ");
-        } else{
-            echo "ERROR: Hush! Sorry $sql. "
-                . mysqli_error($conn);
+
+        $validation_result = ($conn->query($sql));
+        $row = []; 
+
+        if ($result->num_rows > 0) 
+        {
+            
+        } 
+        else{
+            echo "SR-Code exixted, please change the srcode";
         }
+
+        function insertData(){
+            $sql = "INSERT INTO student  VALUES ('$Sr_code','$Lastname','$Firstname','$Middlename','$Address','$ContactNo')";
          
-        // Close connection
-        mysqli_close($conn);
+            echo $sql;
+            if(mysqli_query($conn, $sql)){
+                echo "<h3>data stored in a database successfully."
+                    . " Please browse your localhost php my admin"
+                    . " to view the updated data</h3>";
+    
+                echo nl2br("\n$Sr_code\n $Lastname\n "
+                    . "$Firstname\n $Middlename\n $Address\n $ContactNo\n ");
+            } else{
+                echo "ERROR: Hush! Sorry $sql. "
+                    . mysqli_error($conn);
+            }
+
+             // Performing insert query execution
+            // here our table name is college
+            
+            
+            // Close connection
+            mysqli_close($conn);
+
+        }
+    
+       
         ?>
     </center>
 </body>
